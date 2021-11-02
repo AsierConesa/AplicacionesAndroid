1043082e35b3a1c53534c3c179db62988c4e294b
Asier <asierconesa2002@gmail.com>
2021-11-02 14:39:11 +0100

    corregido error en MultiActivity

--- MultiActitvity/app/src/main/java/com/example/multiactitvity/MainActivity2.java
+++ MultiActitvity/app/src/main/java/com/example/multiactitvity/MainActivity2.java
@@ -15,12 +15,14 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        editTextPersonName = findViewById(R.id.editTextPersonName);
     }
 
     public void volver (View view){
 
         Intent intent = new Intent();
-        intent.putExtra("MESSAGE", editTextPersonName.getText());
+        intent.putExtra("MESSAGE", editTextPersonName.getText().toString());
         setResult(123, intent);
         finish();
 
